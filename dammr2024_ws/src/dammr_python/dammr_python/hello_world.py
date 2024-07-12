962acab75099064fb3a2e8506ac080f718427146
Janusz Jakubiak <Janusz.Jakubiak@pwr.edu.pl>
2024-07-12 12:03:51 +0200

    Examples of a simple node and string publisher

---
+++ dammr2024_ws/src/dammr_python/dammr_python/hello_world.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from dammr_python.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ dammr2024_ws/src/dammr_python/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'dammr_python'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='student',
+    maintainer_email='student@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'hello_world = dammr_python.hello_world:main',
+            'str_publisher = dammr_python.str_publisher:main',
+        ],
+    },
+)
